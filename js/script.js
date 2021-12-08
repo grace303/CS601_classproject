33a3b3f14ac720ed1e92d427f3453508c1d3dcc8
grace303 <yanruz@bu.edu>
2021-12-07 21:32:11 -0800

    add titles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 /*jshint esversion: 6 */
 
 // js for vue
-
 Vue.component('modal', {
     template: '#modal-component'
 });
@@ -9,7 +8,11 @@ Vue.component('modal', {
 new Vue({
     el: '#app',
     data: {
-        showModal: [false, false, false, false, false]
+        showModal: [false, false, false, false, false],
+        portfolioTitle: ["Academus - A research webite", "realtychecklist.com - A website used to do property research",
+            "A budget calculator", "A project at work used to automate email codes"],
+        portfolioURL: ["https://academ.us/", "https://www.realtychecklist.com/",
+            "https://grace303303.github.io/budget/budget/index.html", "https://grace303303.github.io/editorialcreation/"]
     },
     methods: {
         getImgUrl(number) {
