99c09dd2a21a04148f0943069aef391828d6145a
grace303 <yanruz@bu.edu>
2021-12-07 21:55:04 -0800

    add in content

--- js/script.js
+++ js/script.js
@@ -9,10 +9,14 @@ new Vue({
     el: '#app',
     data: {
         showModal: [false, false, false, false, false],
-        portfolioTitle: ["Academus - A research webite", "realtychecklist.com - A website used to do property research",
-            "A budget calculator", "A project at work used to automate email codes"],
         portfolioURL: ["https://academ.us/", "https://www.realtychecklist.com/",
-            "https://grace303303.github.io/budget/budget/index.html", "https://grace303303.github.io/editorialcreation/"]
+            "https://grace303303.github.io/budget/budget/index.html", "https://grace303303.github.io/editorialcreation/"],
+        portfolioTitle: ["Academus.com", "realtychecklist.com",
+            "Budget Calculator", "Email Code Generator"],
+        portfolioContent: ["I led the front-end development from design to implementation including graphics, responsive CSS styling, and interactive Javascript features. Launched this website within a month.",
+            "I developed and launched this website to help first-time homebuyers do property research.",
+            "This is a project I did when learning web development online on my own. By entering income and cost, the user can automatically get budgets.",
+            "This tool will auto-generate email codes for a specific type of email at my work, which highly increased our work efficiency."]
     },
     methods: {
         getImgUrl(number) {
