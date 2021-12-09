33c1487a83ec8ca3aece2641c4484fcf3b5e447a
grace303 <yanruz@bu.edu>
2021-12-08 20:33:46 -0800

    autoplay music

--- js/script.js
+++ js/script.js
@@ -5,9 +5,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // Nav bar
     const hamburger = document.querySelector(".nav-bar");
     const navsub = document.querySelector(".nav-items");
-    hamburger.addEventListener('click', () => {
+    hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("nav-bar-rotate");
         navsub.classList.toggle("nav-show");
     });
-
 });
