af08b62745d483c5b7b645a1a28db81c6555fb37
grace303 <yanruz@bu.edu>
2021-12-08 21:58:14 -0800

    create response msg

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ function toggleNavItem() {
     hamburger.classList.toggle("nav-bar-rotate");
     navsub.classList.toggle("nav-show");
 }
+
+//Contact form
+function submitContact() {
+    //validate input
+    let inputArrs = document.getElementsByClassName("contact-item");
+    for (let input of inputArrs) {
+        if (input.value == '') {
+            document.getElementById("contact-alert").innerHTML = "<p>Please fill in all fields. Thank you!</p>";
+            return;
+        }
+    }
+    //If all fields filled
+    document.getElementById("contact-alert").innerHTML = "";
+    document.getElementById("contact-form").innerHTML = "<p style='color:#fad07a; margin-top: 1rem;'>Your message has been submitted. Thank you!</p>";
+
+}
