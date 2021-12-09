6c9154637e42635bd01b1bdcab59cec4abee176b
grace303 <yanruz@bu.edu>
2021-12-08 20:37:36 -0800

    use onclick for nav

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 /*jshint esversion: 6 */
 
 // Nav bar
-document.addEventListener("DOMContentLoaded", function (event) {
-    // Nav bar
+function toggleNavItem() {
     const hamburger = document.querySelector(".nav-bar");
     const navsub = document.querySelector(".nav-items");
-    hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("nav-bar-rotate");
-        navsub.classList.toggle("nav-show");
-    });
-});
+    hamburger.classList.toggle("nav-bar-rotate");
+    navsub.classList.toggle("nav-show");
+}
