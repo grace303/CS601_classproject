cf6e9ceadcb2bbeab307e6cfbe5b54f09770694d
grace303 <yanruz@bu.edu>
2021-12-08 18:00:05 -0800

    add vue.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,11 @@
 /*jshint esversion: 6 */
 
-// js for vue
-Vue.component('modal', {
-    template: '#modal-component'
-});
-
-new Vue({
-    el: '#app',
-    data: {
-        showModal: [false, false, false, false, false],
-        portfolioURL: ["https://academ.us/", "https://www.realtychecklist.com/",
-            "https://grace303303.github.io/budget/budget/index.html", "https://grace303303.github.io/editorialcreation/"],
-        portfolioTitle: ["Academus.com", "realtychecklist.com",
-            "Budget Calculator", "Email Code Generator"],
-        portfolioContent: ["I led the front-end development from design to implementation including graphics, responsive CSS styling, and interactive Javascript features. Launched this website within a month.",
-            "I developed and launched this website to help first-time homebuyers do property research.",
-            "This is a project I did when learning web development online on my own. By entering income and cost, the user can automatically get budgets.",
-            "This tool will auto-generate email codes for a specific type of email at my work, which highly increased our work efficiency."]
-    },
-    methods: {
-        getImgUrl(number) {
-            return `assets/portfolio_${number}.png`;
-        },
-        showModalFunc(number) {
-            Vue.set(this.showModal, number, true);
-        },
-        closeModalFunc(number) {
-            Vue.set(this.showModal, number, false);
-        }
+function showNav() {
+    let navItems = document.getElementById("nav-items");
+    if (navItems.style.display === "block") {
+        navItems.style.display = "none";
+    } else {
+        navItems.style.display = "block";
     }
-});
+
+}
